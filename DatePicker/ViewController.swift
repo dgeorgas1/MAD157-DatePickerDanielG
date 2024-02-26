710d2852af051101021578cb2a05f5205cbc5a0a
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-02-25 23:22:10 -0600

    Commit changes

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    let dateFormatter: DateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Other styles are medium, long and full
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .short
+    }
+    
+    func ShowAlert(dateTime: String) {
+        let alert = UIAlertController(title: "Selected Date and Time", message: "\(dateTime)", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {
+            action -> Void in
+        })
+        
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
     }
 
-
+    @IBAction func getCurrentDateTime(_ sender: UIButton) {
+        let selectedDate: String = dateFormatter.string(from: myDatePicker.date)
+        
+        ShowAlert(dateTime: selectedDate)
+    }
+    
+    @IBAction func dateChanged(_ sender: UIDatePicker) {
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        
+        ShowAlert(dateTime: selectedDate)
+    }
 }
-
